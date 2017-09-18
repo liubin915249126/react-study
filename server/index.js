146246ddcdd9dfa501691211dbdd81863da761a7
liubin <915249126@qq.com>
2017-09-18 10:47:10 +0800

    新增文件

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const Koa = require('koa');
+const router = require('koa-router')();
+const app = new Koa();
+
+
+router.get('/', function (ctx, next) {
+    ctx.response.body = '111'
+});
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
+
+app.listen(3000);
+console.log('server is start')
