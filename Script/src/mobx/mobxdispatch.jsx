074fe1f896376ba16bfa56f9e6e100088707bd01
liubin <915249126@qq.com>
2017-12-26 10:00:46 +0800

    引入mobx

--- Script/src/mobx/mobxdispatch.jsx
+++ Script/src/mobx/mobxdispatch.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import {Button} from 'antd';
+import {Button,Input} from 'antd';
 import { observer,inject } from 'mobx-react'
 
 @inject('clickTimes') @observer class MobxDispatch extends React.Component{
     constructor(props){
         super(props)
+        this.state={
+            num:1
+        }
     }
     handleClick(){
-        this.props.clickTimes.click(1);
+        let {num} = this.state;
+        this.props.clickTimes.click(parseInt(num));
+    }
+
+    handleChange(e){
+       let {value} = e.target;
+       this.setState({num:value})          
     }
     render(){
+        let {num} = this.state;
         return(<div className="mobxdispatch">
-            <Button onClick={()=>{this.handleClick()}}>点击</Button>
+            <Button type="primary" onClick={()=>{this.handleClick()}}>点击</Button>
+            <Input onChange={(e)=>{this.handleChange(e)}} value={num} />
         </div>)
     }
 }
