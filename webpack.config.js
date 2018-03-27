1b37bde1219a9214374e5c40ded5baaf9aae9064
刘宾 <915249126@qq.com>
2018-03-27 14:49:37 +0800

    add env

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ const config = {
     devServer: {
         contentBase: "./Script",//本地服务器所加载的页面所在的目录
         historyApiFallback: true,
-        port:8082,
+        port:8083,
         proxy:{
             '/': { target: 'http://localhost:3000', secure: false }
         }
