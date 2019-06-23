25d20c7346549e460e0c273e38e53c84e4af5745
liubin <915249126@qq.com>
2019-06-23 12:24:50 +0800

    add baidu map

--- src/views/login/index.jsx
+++ src/views/login/index.jsx
@@ -14,6 +14,46 @@ class LoginView extends React.Component{
             }
         }
     }
+    componentDidMount(){
+      this.initMap()
+    }
+    initMap(){
+      var map = new BMap.Map("container"); 
+      var point = new BMap.Point(116.404, 39.915); 
+      map.centerAndZoom(point, 15); //
+      map.enableScrollWheelZoom(true); //允许缩放
+      map.addControl(new BMap.NavigationControl()); //添加控件
+      var geolocation = new BMap.Geolocation();
+      geolocation.getCurrentPosition(function(r){ //浏览器定位
+        if(this.getStatus() == BMAP_STATUS_SUCCESS){
+          var mk = new BMap.Marker(r.point);
+          map.addOverlay(mk);
+          map.panTo(r.point);
+          console.log('您的位置：'+r.point.lng+','+r.point.lat);
+        }
+        else {
+          console.log('failed'+this.getStatus());
+          geolocation.enableSDKLocation();  //SDK定位
+          geolocation.getCurrentPosition(function(r){
+            if(this.getStatus() == BMAP_STATUS_SUCCESS){
+              var mk = new BMap.Marker(r.point);
+              map.addOverlay(mk);
+              map.panTo(r.point);
+              console.log('您的位置：'+r.point.lng+','+r.point.lat);
+            }
+            else {
+              console.log('failed'+this.getStatus());
+              var myCity = new BMap.LocalCity();
+              myCity.get(()=>{
+                var cityName = result.name;
+                map.setCenter(cityName);
+                console.log("当前定位城市:"+cityName);
+              }); 
+            }        
+          });
+        }        
+      });
+    }
     loginIn(){
         this.props.history.push('/main')
     }
@@ -34,32 +74,35 @@ class LoginView extends React.Component{
         };
         return(
         <div className="loginWrap">
-          <Form>
-            <h3>欢迎登陆系统</h3>  
-            <FormItem {...formItemLayout} label="姓名">
-            {getFieldDecorator('name', { rules: [{ required: true, message: '请输入姓名' }],initialValue:userinfo.name})(
-                <Input />
-            )}
-            </FormItem>
-            <FormItem {...formItemLayout} label="密码">
-              {getFieldDecorator('password', { rules: [{ required: true, message: '请输入密码' }],initialValue:userinfo.password})(
-                <Input type="password"/>
+          <div className="formWrap">
+            <Form>
+              <h3>欢迎登陆系统</h3>  
+              <FormItem {...formItemLayout} label="姓名">
+              {getFieldDecorator('name', { rules: [{ required: true, message: '请输入姓名' }],initialValue:userinfo.name})(
+                  <Input />
               )}
-            </FormItem>
-            <Row>
-                <Col {...formItemLayout.labelCol}></Col>
-                <Col {...formItemLayout.wrapperCol}>
-                    <Button
-                      onClick={()=>{this.loginIn()}} 
-                      type="primary"
-                    >
-                      登录
-                    </Button>
-                </Col>
-            </Row>
-          </Form>
-          <div>
-            
+              </FormItem>
+              <FormItem {...formItemLayout} label="密码">
+                {getFieldDecorator('password', { rules: [{ required: true, message: '请输入密码' }],initialValue:userinfo.password})(
+                  <Input type="password"/>
+                )}
+              </FormItem>
+              <Row>
+                  <Col {...formItemLayout.labelCol}></Col>
+                  <Col {...formItemLayout.wrapperCol}>
+                      <Button
+                        onClick={()=>{this.loginIn()}} 
+                        type="primary"
+                      >
+                        登录
+                      </Button>
+                  </Col>
+              </Row>
+            </Form>
+          </div>
+          <div className="container">
+            <span>地图定位</span>
+            <div id="container"></div> 
           </div>
         </div>)
     }
