e34425acc534529fa1b803bd4b0aca0e60650cf2
liubin <915249126@qq.com>
2019-12-22 10:51:34 +0800

    modify model

--- src/views/test/index.jsx
+++ src/views/test/index.jsx
@@ -3,10 +3,14 @@ import React from 'react';
 import asap from '../../promise/browser-asap'
 // import asap  from 'asap/raw'
 import MyPromise from '../../promise/Promise'
+import { Button } from 'antd';
 
 export default class Test extends React.Component{
     constructor(props){
         super(props);
+        this.state={
+            count:0
+        };
     }
     componentDidMount(){
         this.testPromise()
@@ -29,10 +33,20 @@ export default class Test extends React.Component{
             console.log('setTimeout')
         },0)
     }
+    testSetState(){
+        this.setState({count:1})
+        console.log(111,this.state.count)
+        new Promise(resolve =>{ resolve()}).then(()=>{
+            this.setState({count:1})
+            console.log(222,this.state.count)
+        })
+        this.setState(prevState => {count: prevState.count + 1});
+        console.log(333,this.state.count)
+    }
     render(){
         return (
             <div>
-
+               <Button type="primary" onClick={() => this.testSetState()}>setState测试</Button>
             </div>
         )
     }
